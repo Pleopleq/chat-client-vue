58544b7ca0b4dab8368fdd1bceabd3a67a1e3faa
Felix Anducho <felix.anducho74@gmail.com>
2022-08-09 20:53:26 -0500

    Add vue router to the project

--- src/components/TheHome/index.vue
+++ src/components/TheHome/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>LOGIN</h1>
+  <h1>LOGINsadfdssdfsa</h1>
 </template>
 
 <script>
---
+++ src/router/routes.ts
@@ -0,0 +1,10 @@
+import TheHome from "../components/TheHome/index.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: TheHome,
+  },
+];
+
+export default routes;
