411aaaa9321c25c9d0e1ae619cd71356d6a1d3f4
Felix Anducho <felix.anducho74@gmail.com>
2022-08-09 20:29:48 -0500

    Add initial files

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import TheLogin from "./components/TheLogin/index.vue";
+</script>
+
+<template>
+  <TheLogin msg="Vite + Vue" />
+</template>
+
+<style scoped></style>
