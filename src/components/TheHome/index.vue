808cdbf5cf9d1dcadfeff38c9084f97d628fe941
Felix Anducho <felix.anducho74@gmail.com>
2022-08-15 20:08:04 -0500

    ✨Add markup to TheHome component. Create register page

--- src/components/TheHome/index.vue
+++ src/components/TheHome/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <h1>LOGINsadfdssdfsa</h1>
+  <section>
+    <div>
+      <h1>PleqChat</h1>
+      <h3>A group chat just like the old mIRC days 💬</h3>
+    </div>
+
+    <div>
+      <form action="">
+        <input type="text" placeholder="Username" />
+
+        <input type="password" placeholder="Password" />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+
+    <div>
+      <router-link to="/register">Register</router-link>
+    </div>
+  </section>
 </template>
 
 <script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,14 @@
 import TheHome from "../components/TheHome/index.vue";
+import TheRegisterPage from "../components/TheRegisterPage/index.vue";
 
 const routes = [
   {
     path: "/",
     component: TheHome,
+  },
+  {
+    path: "/register",
+    component: TheRegisterPage,
   },
 ];
 
